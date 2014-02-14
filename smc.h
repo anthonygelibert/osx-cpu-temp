d8f37e52eec07a4cfc2290a37f18502000554d11
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-14 14:57:16 +0100

    Clean smc.h.

--- smc.h
+++ smc.h
@@ -21,32 +21,15 @@
 #define __SMC_H__
 #endif
 
+#include <IOKit/IOKitLib.h>
+
 #ifndef __SMC_C__
 #define PUBLIC extern
 #else
 #define PUBLIC
 #endif
 
-#define SMC_VERSION           "1.0.0"
-
-#define KERNEL_INDEX_SMC      2
-
-#define SMC_CMD_READ_BYTES    5
-#define SMC_CMD_WRITE_BYTES   6
-#define SMC_CMD_READ_INDEX    8
-#define SMC_CMD_READ_KEYINFO  9
-#define SMC_CMD_READ_PLIMIT   11
-#define SMC_CMD_READ_VERS     12
-
-#define DATATYPE_FPE2         "fpe2"
-#define DATATYPE_UINT8        "ui8 "
-#define DATATYPE_UINT16       "ui16"
-#define DATATYPE_UINT32       "ui32"
-#define DATATYPE_SP78         "sp78"
-
-// key values
-#define SMC_KEY_CPU_TEMP      "TC0P"
-
+#define SMC_KEY_CPU_TEMP      "TC0D"
 
 typedef struct {
     char                  major;
@@ -70,7 +53,6 @@ typedef struct {
     char                  dataAttributes;
 } SMCKeyData_keyInfo_t;
 
-typedef char              SMCBytes_t[32];
 
 typedef struct {
   UInt32                  key;
@@ -84,7 +66,6 @@ typedef struct {
   SMCBytes_t              bytes;
 } SMCKeyData_t;
 
-typedef char              UInt32Char_t[5];
 
 typedef struct {
   UInt32Char_t            key;
@@ -94,6 +75,8 @@ typedef struct {
 } SMCVal_t;
 
 
+PUBLIC kern_return_t SMCOpen(void);
 PUBLIC double SMCGetTemperature(char *key);
+PUBLIC kern_return_t SMCClose(void);
 
 #undef PUBLIC
